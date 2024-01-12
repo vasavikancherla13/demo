925d741e2ebbe9722c163b2af9ab60f7dedd05b9
kancharla vasavi <147906829+vasavikancherla13@users.noreply.github.com>
2024-01-12 11:03:01 +0530

    Update shinchan.py

--- shinchan.py
+++ shinchan.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.header('shinchan')
-st.write('shinchan')
-st.write('nom nom nommmm')
-st.image('https://pbs.twimg.com/media/E4DfwDMVoAIAJJe.jpg:large',caption='shinchan')
+st.header('HELLO !')
+st.write('eat 5 star do nothing')
+st.image('https://res.cloudinary.com/teepublic/image/private/s--49c7B2E1--/c_crop,x_10,y_10/c_fit,h_1109/c_crop,g_north_west,h_1260,w_1260,x_-147,y_-76/co_rgb:c8e0ec,e_colorize,u_Misc:One%20Pixel%20Gray/c_scale,g_north_west,h_1260,w_1260/fl_layer_apply,g_north_west,x_-147,y_-76/bo_157px_solid_white/e_overlay,fl_layer_apply,h_1260,l_Misc:Art%20Print%20Bumpmap,w_1260/e_shadow,x_6,y_6/c_limit,h_1254,w_1254/c_lpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1586958026/production/designs/9135895_0.jpg',caption='hello from cat')
