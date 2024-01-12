50bed08cb207bfe859f0859c5acd5e3bee4c65c2
kancharla vasavi <147906829+vasavikancherla13@users.noreply.github.com>
2024-01-12 10:41:48 +0530

    Update shinchan.py

--- shinchan.py
+++ shinchan.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.header('shinchan')
 st.write('shinchan')
-st.image('https://pbs.twimg.com/media/E4DfwDMVoAIAJJe.jpg:large',caption='nom nom nom ')
+st.write('nom nom nommmm')
+st.image('https://pbs.twimg.com/media/E4DfwDMVoAIAJJe.jpg:large',caption='shinchan')
